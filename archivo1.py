8f3ec2c8b9915070854d2bdcf8483a3010906d22
Gabito <ares74849@gmail.com>
2026-03-30 16:57:11 -0300

    voy integrando mi ruleta

--- archivo1.py
+++ archivo1.py
@@ -1,2 +1,33 @@
-print ("Bienvenidos a Snickers´s Game")
-print ("¿Que juego desea jugar?")
+from ruleta import jugar_ruleta
+
+import os
+
+
+def limpiar_pantalla():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def menu():
+    limpiar_pantalla()
+    while True:
+        print("=" * 40)
+        print(" CASINO ".center(40))
+        print("=" * 40)
+        print("1. Ruleta")
+        print("2. Blackjack")
+        print("0. Salir")
+
+        opcion = input("Elegí una opción: ")
+
+        if opcion == "1":
+            jugar_ruleta()
+        
+        elif opcion == "0":
+            print("Gracias por jugar!")
+            break
+        else:
+            print("Opción inválida")
+
+
+if __name__ == "__main__":
+    menu()
