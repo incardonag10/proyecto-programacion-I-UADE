c41050128d8f36c90c9823618a731b71eb4c0185
Gabito <ares74849@gmail.com>
2026-05-21 15:00:48 -0300

    corrijo menu principal

--- archivo1.py
+++ archivo1.py
@@ -1,16 +1,12 @@
-from ruleta import jugar_ruleta
-from blackjack import jugar_blackjack
-
-
-
 def registrar_jugador():
     
     print("======================================")
     print("        REGISTRO DEL CASINO           ")
     print("======================================")
-
-    
+  
     errores = 0
+
+    #corregir salir si soy menor, parar apenas pongo nombre o apellido mal y salir si el usuario agoto los intentos
 
     while errores < 3:
 
@@ -49,7 +45,7 @@ def registrar_jugador():
         elif edad < 18:
             print("Acceso denegado. Debe ser mayor de 18 años.")
             input("Presioná ENTER para salir...")
-            exit()
+            
 
         else:
             nombre = nombre.capitalize()
@@ -58,21 +54,15 @@ def registrar_jugador():
             print("Bienvenido", nombre, apellido)
             break
 
-            
-
     if errores == 3:
         print("El usuario agotó los intentos. Lo siento, no puede jugar.")
 
-  
-    
 
     return nombre, apellido, edad
 
 
 def menu():
     nombre, apellido, edad = registrar_jugador()
-
-    
 
     saldo = 1000
 
@@ -87,6 +77,8 @@ def menu():
 
         print("1. Ruleta")
         print("2. Blackjack")
+        print("3. Generala")
+        print("4. Poker")
         print("0. Salir")
 
         opcion = input("Elegí una opción: ")
@@ -97,6 +89,12 @@ def menu():
         elif opcion == "2":
             saldo = jugar_blackjack(nombre, apellido, saldo)
 
+        elif opcion == "3":
+            saldo = jugar_generala (nombre, apellido, saldo)
+
+        elif opcion == "4":
+            saldo = jugar_poker (nombre, apellido, saldo)
+
         elif opcion == "0":
             print("Gracias por jugar!")
             break
@@ -104,7 +102,3 @@ def menu():
         else:
             print("Opción inválida")
             input("Presioná ENTER para continuar...")
-
-
-if __name__ == "__main__":
-    menu()
