13f6e0a4dc48fe700dd02e4f5c3834f09b31d12e
Gabito <ares74849@gmail.com>
2026-06-11 14:22:39 -0300

    agrego archivos

--- archivo1.py
+++ archivo1.py
@@ -1,3 +1,9 @@
+from blackjack import jugar_blackjack
+
+from ruleta import jugar_ruleta
+
+
+
 def registrar_jugador():
     
     print("======================================")
@@ -6,99 +12,112 @@ def registrar_jugador():
   
     errores = 0
 
-    #corregir salir si soy menor, parar apenas pongo nombre o apellido mal y salir si el usuario agoto los intentos
-
     while errores < 3:
 
         nombre = input("Ingrese su nombre: ")
+
+        if nombre == "":
+            print("El nombre no puede estar vacío")
+            errores += 1
+
+        elif " " in nombre:
+            print("Ingrese un solo nombre")
+            errores += 1
+
+        else:
+            nombre = nombre.capitalize()
+            break
+
+
+    while errores < 3:
+
         apellido = input("Ingrese su apellido: ")
-        
+
+        if apellido == "":
+            print("El apellido no puede estar vacío")
+            errores += 1
+
+        elif " " in apellido:
+            print("Ingrese un solo apellido")
+            errores += 1
+
+        else:
+            apellido = apellido.capitalize()
+            break
+
+
+    while errores < 3:
+
         try:
             edad = int(input("Ingrese su edad: "))
+
+            if edad <= 0:
+                print("Edad inválida")
+                errores += 1
+
+            elif edad < 18:
+                print("Acceso denegado. Debe ser mayor de 18 años.")
+                return None, None, None
+
+            else:
+                break
+
         except ValueError:
             print("Edad inválida")
             errores += 1
 
-        if nombre == "":
-            print("El nombre no puede estar vacíos")
-            print("Por favor, ingrese un nombre válido")
-            errores += 1
-        
-        elif " " in nombre:
-            print("Por favor, ingrese un solo nombre.")
-            errores += 1
-
-        elif apellido == "":
-            print("El apellido no puede estar vacío")
-            print("Por favor, ingrese un apellido válido")
-            errores += 1
-
-        elif " " in apellido:
-            print("Por favor, ingrese un solo apellido.")
-            errores += 1
-
-        elif edad <= 0:
-            print("Edad inválida")
-            print("Por favor, ingrese una edad válida")
-            errores += 1
-
-        elif edad < 18:
-            print("Acceso denegado. Debe ser mayor de 18 años.")
-            input("Presioná ENTER para salir...")
-            
-
-        else:
-            nombre = nombre.capitalize()
-            apellido = apellido.capitalize()
-            print("Registro exitoso")
-            print("Bienvenido", nombre, apellido)
-            break
 
     if errores == 3:
-        print("El usuario agotó los intentos. Lo siento, no puede jugar.")
+        print("Lo siento agoto los intentos. No puede jugar.")
+        return None, None, None
 
+    print("Registro exitoso")
+    print("Bienvenido", nombre, apellido)
 
     return nombre, apellido, edad
-
-
 def menu():
+    
     nombre, apellido, edad = registrar_jugador()
 
-    saldo = 1000
+    if nombre != None and apellido != None and edad != None:
+        saldo = 1000
 
-    while True:
-        print("========================================")
-        print("                CASINO                  ")
-        print("========================================")
+        while True:
+            print("========================================")
+            print("                CASINO                  ")
+            print("========================================")
 
-        print(f"Jugador: {nombre} {apellido}")
-        print(f"Edad: {edad}")
-        print(f"Saldo general: ${saldo}")
+            print(f"Jugador: {nombre} {apellido}")
+            print(f"Edad: {edad}")
+            print(f"Saldo general: ${saldo}")
 
-        print("1. Ruleta")
-        print("2. Blackjack")
-        print("3. Generala")
-        print("4. Poker")
-        print("0. Salir")
+            print("1. Ruleta")
+            print("2. Blackjack")
+            print("3. Generala")
+            print("4. Poker")
+            print("0. Salir")
 
-        opcion = input("Elegí una opción: ")
+            opcion = input("Elegí una opción: ")
 
-        if opcion == "1":
-            saldo = jugar_ruleta(nombre, apellido, saldo)
+            if opcion == "1":
+                saldo = jugar_ruleta (nombre, apellido, saldo)
 
-        elif opcion == "2":
-            saldo = jugar_blackjack(nombre, apellido, saldo)
+            elif opcion == "2":
+                saldo = jugar_blackjack (nombre, apellido, saldo)
 
-        elif opcion == "3":
-            saldo = jugar_generala (nombre, apellido, saldo)
+            elif opcion == "3":
+                saldo = jugar_generala (nombre, apellido, saldo)
 
-        elif opcion == "4":
-            saldo = jugar_poker (nombre, apellido, saldo)
+            elif opcion == "4":
+                saldo = jugar_poker (nombre, apellido, saldo)
 
-        elif opcion == "0":
-            print("Gracias por jugar!")
-            break
+            elif opcion == "0":
+                print("Gracias por jugar!")
+                break
 
-        else:
-            print("Opción inválida")
-            input("Presioná ENTER para continuar...")
+            else:
+                print("Opción inválida")
+                input("Presioná ENTER para continuar...")
+
+if __name__ == "__main__":
+    menu()
