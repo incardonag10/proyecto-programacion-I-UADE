5a55298246947a87e0aaa6477c6df4798b5c7094
AgusGraña <agustingrana51@gmail.com>
2026-06-11 13:24:40 -0300

    añado otros asserts

--- test_cartas.py
+++ test_cartas.py
@@ -10,3 +10,13 @@ cartas_bot = repartir_cartas(cartas, 2)
 todas = cartas_jugador + cartas_bot + cartas_en_mesa
 
 assert len(todas) == len(set(todas))
+
+from poker import generar_cartas, repartir_cartas, cartas_en_la_mesa_ronda
+cartas_en_mesa = repartir_cartas(cartas, 3)
+cartas_en_la_mesa_ronda(cartas, cartas_en_mesa)
+
+assert len(cartas_en_mesa) == 4
+
+cartas_en_la_mesa_ronda(cartas, cartas_en_mesa)
+
+assert len(cartas_en_mesa) == 5
