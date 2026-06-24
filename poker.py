577e442d7dbfcb9c15c51d737742c3bf3c213153
AgusGraña <agustingrana51@gmail.com>
2026-06-24 01:20:41 -0300

    cambio banco por una variable sola

--- poker.py
+++ poker.py
@@ -1,4 +1,4 @@
-def jugar_poker(nombre,apellido,estado):
+def jugar_poker(nombre,apellido,banco):
     import random
 
     while True:
@@ -9,7 +9,8 @@ def jugar_poker(nombre,apellido,estado):
                 print("Entrada inválida. Ingrese si o no.")
 
     def main():
-        apuesta = cargar_apuesta(estado)
+        nonlocal banco
+        apuesta = cargar_apuesta()
         cartas = generar_cartas()
         cartas_jugador = repartir_cartas(cartas, 2)
         cartas_en_mesa = cartas_en_la_mesa(cartas)
@@ -26,7 +27,7 @@ def jugar_poker(nombre,apellido,estado):
             print("Entrada inválida. Ingrese si o no.")
             apuesta2 = input("Desea apostar mas? (si o no): ")
         if apuesta2.lower() == "si":
-            apuesta_extra = cargar_apuesta(estado)
+            apuesta_extra = cargar_apuesta()
             apuesta += apuesta_extra
 
 
@@ -41,7 +42,7 @@ def jugar_poker(nombre,apellido,estado):
             print("Entrada inválida. Ingrese si o no.")
             apuesta3 = input("Desea apostar mas? (si o no): ")
         if apuesta3.lower() == "si":
-            apuesta_extra1 = cargar_apuesta(estado)
+            apuesta_extra1 = cargar_apuesta()
             apuesta += apuesta_extra1
             
 
@@ -59,7 +60,7 @@ def jugar_poker(nombre,apellido,estado):
             print("Entrada inválida. Ingrese si o no.")
             apuesta4 = input("Desea apostar mas? (si o no): ")
         if apuesta4.lower() == "si":
-            apuesta_extra2 = cargar_apuesta(estado)
+            apuesta_extra2 = cargar_apuesta()
             apuesta += apuesta_extra2
             
 
@@ -70,7 +71,7 @@ def jugar_poker(nombre,apellido,estado):
         print("---------------------------------------------------------------------" )
         print(f"            Clasificación tuya: {clasificacion_cartas(cartas_en_mesa, cartas_jugador)}")
         print("---------------------------------------------------------------------" )
-        print(ganador(cartas_en_mesa, cartas_jugador, cartas_bot,estado,apuesta))
+        print(ganador(cartas_en_mesa, cartas_jugador, cartas_bot,apuesta))
         print("---------------------------------------------------------------------" )
 
 
@@ -81,7 +82,7 @@ def jugar_poker(nombre,apellido,estado):
             main()
 
         elif Devuelta.lower() == "no":
-            print(f"Desea añadir mas fondos al banco? Actualmente tenes ${estado['banco']} en el banco")
+            print(f"Desea añadir mas fondos al banco? Actualmente tenes ${banco} en el banco")
             respuesta_banco = input("Ingrese si o no: ")
 
             if respuesta_banco.lower() == "si":
@@ -90,7 +91,7 @@ def jugar_poker(nombre,apellido,estado):
                     try: 
                         monto = float(input("Ingrese monto a depositar en el banco: $"))
                         if monto > 0:
-                            estado["banco"] += monto
+                            banco += monto
                             ok = True
                             main()
                         else:
@@ -100,14 +101,15 @@ def jugar_poker(nombre,apellido,estado):
             else:
                 print("Gracias por jugar Snicker´s poker! Vuelva pronto!")
     #Funcion para cargar la apuesta hasta q sea valida
-    def cargar_apuesta (estado):
+    def cargar_apuesta ():
+        nonlocal banco
         while True:
             try:
                 apuesta = float(input("Elija monto a apostar: $"))
-                while apuesta > estado["banco"]:
-                    print(f"No hay fondos suficientes, tenes ${estado['banco']} en el banco")
+                while apuesta > banco:
+                    print(f"No hay fondos suficientes, tenes ${banco} en el banco")
                     apuesta = float(input("Elija monto a apostar: $"))
-                estado["banco"] -= apuesta
+                banco -= apuesta
                 return apuesta
             except:
                 print("Entrada inválida. Ingrese un número.")
@@ -210,7 +212,8 @@ def jugar_poker(nombre,apellido,estado):
         
         
     #Toma la cartas de la mesa y los jugadores para ver cual es la mas alta en caso de empate se usa esta funcion 
-    def desempate(cartas_en_mesa, cartas_jugador, cartas_bot,estado,apuesta):
+    def desempate(cartas_en_mesa, cartas_jugador, cartas_bot,apuesta):
+        nonlocal banco
         todas_las_cartas_jugador = cartas_en_mesa + cartas_jugador
         todas_las_cartas_bot = cartas_en_mesa + cartas_bot
         carta_mayor_jugador = max(todas_las_cartas_jugador)
@@ -223,13 +226,13 @@ def jugar_poker(nombre,apellido,estado):
             desempate=0
 
         if desempate == 1:
-            estado["banco"] += apuesta*2
-            return f"            Gana el jugador Fondos restantes en el banco: $ {estado['banco']}"
+            banco += apuesta*2
+            return f"            Gana el jugador Fondos restantes en el banco: $ {banco}"
         elif desempate == 2:
-            return f"            Gana el bot  Fondos restantes en el banco: $ {estado['banco']}"
+            return f"            Gana el bot  Fondos restantes en el banco: $ {banco}"
         else:
-            estado["banco"] += apuesta
-            return f"            Empate por la carta mas alta Fondos restantes en el banco: $ {estado['banco']}"
+            banco += apuesta
+            return f"            Empate por la carta mas alta Fondos restantes en el banco: $ {banco}"
         
 
     #asigna cada simbolo escirto a el simbolo en si
@@ -272,24 +275,25 @@ def jugar_poker(nombre,apellido,estado):
 
 
     #Ponemos una lista del orden de clasificacion y segun las posiciones de ese orden va a dar si es mayor o menor q las otras
-    def ganador(cartas_en_mesa, cartas_jugador, cartas_bot, estado,     apuesta):
+    def ganador(cartas_en_mesa, cartas_jugador, cartas_bot, apuesta):
+        nonlocal banco
         clasificacion_jugador = clasificacion_cartas(cartas_en_mesa, cartas_jugador)
         clasificacion_bot = clasificacion_cartas(cartas_en_mesa, cartas_bot)
 
         orden_clasificaciones = ["Carta alta", "Pareja", "Doble pareja", "Triple", "Escalera", "Color", "Full House", "Poker"]
         if orden_clasificaciones.index(clasificacion_jugador) > orden_clasificaciones.index(clasificacion_bot):
-            estado["banco"] += apuesta*2 
-            return f"            Gana el jugador Fondos restantes en el banco: $ {estado['banco']}"
+            banco += apuesta*2 
+            return f"            Gana el jugador Fondos restantes en el banco: $ {banco}"
         elif orden_clasificaciones.index(clasificacion_jugador) < orden_clasificaciones.index(clasificacion_bot):
-            return f"            Gana el bot  Fondos restantes en el banco: $ {estado['banco']}"
+            return f"            Gana el bot  Fondos restantes en el banco: $ {banco}"
         else:
-            return desempate(cartas_en_mesa, cartas_jugador, cartas_bot,estado,apuesta)
+            return desempate(cartas_en_mesa, cartas_jugador, cartas_bot,apuesta)
 
 
     if arranque == "si":
         main()
     
-    return estado["banco"]
+    return banco
     
 
 
